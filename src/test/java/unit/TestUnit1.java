152aef7b9fbce9fd465efa3ab22da9e9b42e52f4
Rockie Yang <eyouyan@gmail.com>
2016-05-03 19:25:09 +0200

    workable testng integration framework

---
+++ src/test/java/unit/TestUnit1.java
@@ -0,0 +1,11 @@
+package unit;
+
+import org.testng.annotations.Test;
+
+public class TestUnit1 {
+
+	@Test
+	public void unit1() {
+		System.out.println("unit test 1");
+	}
+}
---
+++ src/test/java/unit/TestUnit2.java
@@ -0,0 +1,11 @@
+package unit;
+
+import org.testng.annotations.Test;
+
+public class TestUnit2 {
+
+	@Test
+	public void unit2() {
+		System.out.println("unit test 2");
+	}
+}
